2c08fd601386a85d42cecb5cb9c64776876a0592
isaiasNsantos <isaiasamaisa15@gmail.com>
2024-04-03 15:12:45 -0300

    novas aulas bd

---
+++ 2-tebela_sqlite.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+# 1  -conectando no BD
+
+connection = sqlite3.connect('title.db')
+
+# sempre que for preciso manipular os dados - DDL OU DNL
+# 2- Criando UM CURSOR
+'''
+é um interador que permite navegar e 
+manipular os registros em um BD
+'''
+curso = connection.cursor()
+
+# 3 - Criando a tabela
+curso.execute('''
+    CREATE TABLE movies(
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        year INTEGER NOT NULL,
+        score REAL NOT NULL
+    );
+        ''')
+# 4 fechando a conexção
+print('Tabela criada com secesso')
+connection.close()
